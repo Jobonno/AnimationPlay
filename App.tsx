514d7abf436d155b610ff83797bb9c86607f8775
Joseph Bonifacio <joseph.bonifacio@service.nsw.gov.au>
2020-01-06 11:04:48 +1100

    Implement components on the app screen

--- App.tsx
+++ App.tsx
@@ -8,21 +8,80 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import AnimatedTextInput from "./Components/AnimatedTextInput";
-import {Dimensions} from "react-native";
+import {Dimensions, Text, StyleSheet, TouchableOpacity, View} from "react-native";
 import Tabber from "./Components/Tabber";
+import Spread from "./Components/spread";
+import ParralaxModal from "./Components/parralaxModal";
 
 const {width} = Dimensions.get('window');
 
+
 const App = () => {
+    const [navIndex, setNavIndex] = useState(0);
+    const [menuState, setmenuState] = useState(true);
+
+    function nav() {
+        switch (navIndex) {
+            case 0 :
+                return <Tabber/>
+            case 1:
+                return <Spread/>
+            case 2:
+                return <ParralaxModal/>
+            case 3:
+                return <AnimatedTextInput width={width - 40} height={250}/>
+
+            default:
+                return <></>
+
+        }
+    }
+
     return (
-        <>
-            <Tabber/>
-            <AnimatedTextInput width={width - 40} height={250}/>
-        </>
+        <View style={{flex: 1}}>
+            {menuState &&
+            <View style={styles.navBar}>
+                <TouchableOpacity
+                    onPress={() => setNavIndex(0)}
+                    style={styles.navButton}>
+                    <Text>ONE</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => setNavIndex(1)}
+                    style={styles.navButton}>
+                    <Text>TWO</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => setNavIndex(2)}
+                    style={styles.navButton}>
+                    <Text>THREE</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => setNavIndex(3)}
+                    style={styles.navButton}>
+                    <Text>FOUR</Text>
+                </TouchableOpacity>
+            </View>
+            }
+            {nav()}
+        </View>
     )
 };
 
+const styles = StyleSheet.create({
+    navBar: {
+        flexDirection: 'row',
+        height: 60,
+        marginTop: 40
+    },
+    navButton: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 1
+    }
+});
 
 export default App;
